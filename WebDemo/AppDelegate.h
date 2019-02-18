0dd43b27301b464f8e93d88a97546a7be9a80242
GN_木头人 <946193683@.com>
2019-02-18 17:57:58 +0800

    Initial Commit

---
+++ WebDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebDemo
+//
+//  Created by CaptainMa on 2019/2/18.
+//  Copyright © 2019 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebDemo
+//
+//  Created by CaptainMa on 2019/2/18.
+//  Copyright © 2019 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
